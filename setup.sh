bf78a61fa1e5613fa2c41de388b1f3b377d185b5
Jun Ye <yetsun@gmail.com>
2014-09-23 14:06:23 -0700

    update git config

---
+++ setup.sh
@@ -0,0 +1,10 @@
+git config --global user.email "yetsun@gmail.com"
+git config --global user.name "Jun Ye"
+
+
+#update packages
+sudo apt-get update
+
+#install nodejs
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
