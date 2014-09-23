ce29c786ea2e3b26c8294700d0835c1bbc9de644
Jun Ye <yetsun@gmail.com>
2014-09-23 14:39:11 -0700

    install/config vim

--- README.md
+++ README.md
@@ -7,6 +7,5 @@ This is the setup file for ubuntu environment
 ```
 sudo apt-get install git-core
 git clone https://github.com/yetsun/ubuntu_setup.git
-cd ubuntu_setup
-sudo ./setup.sh
+sudo ./ubuntu_setup/setup.sh
 ```
--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
+#config git
 git config --global user.email "yetsun@gmail.com"
 git config --global user.name "Jun Ye"
+
+#install vim
+sudo apt-get -y install vim
+cp ubuntu_setup/.vimrc ~/
+touch ~/.vimrc
+
 
 
 #update packages
