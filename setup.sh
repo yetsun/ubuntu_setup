37cfb6826cbe8c539561533ca993732187cb40a8
Jun Ye <yetsun@gmail.com>
2014-09-25 21:08:58 -0700

    add mongodb

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+#config apt-get for mangodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+#update packages
+sudo apt-get update
+
+
+
 #config git
 git config --global user.email "yetsun@gmail.com"
 git config --global user.name "Jun Ye"
@@ -8,10 +17,15 @@ cp ubuntu_setup/.vimrc ~/
 touch ~/.vimrc
 
 
-
-#update packages
-sudo apt-get update
-
 #install nodejs
 sudo apt-get -y install nodejs
 sudo apt-get -y install npm
+
+
+
+#install mongodb
+sudo apt-get install -y mongodb-org
+
+
+
+
